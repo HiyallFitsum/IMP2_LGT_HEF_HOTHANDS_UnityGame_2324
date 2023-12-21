3c6f5e14e71d571897d35e96a157df10f9e40746
HiyallFitsum <70286522+HiyallFitsum@users.noreply.github.com>
2023-12-21 14:30:09 -0500

    Add files via upload

---
+++ Henchman/HEF_HenchmanMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HEF_HenchmanMovement : MonoBehaviour
+{
+    public Transform henchman;
+    public Transform henchmanPos;
+    private float duration = 30.0f;
+    public float startTime;
+    public Transform distance;
+    public float t = 0;
+    public bool Moving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        t = (Time.time - startTime) / duration;
+        if(Moving){
+            duration = 30.0f;
+            Debug.Log("Henchman is not colliding (SmoothStep) ");
+            henchman.position = new Vector3(Mathf.SmoothStep(henchman.position.x, henchmanPos.position.x, t), Mathf.SmoothStep(henchman.position.y, henchmanPos.position.y, t), Mathf.SmoothStep(henchman.position.z, henchmanPos.position.z, t));
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.CompareTag("Henchman")){
+            Debug.Log("Henchman is in collider (MoveTowards) ");
+            Moving = false;
+            duration = 1000.0f;
+            henchman.position = new Vector3(Mathf.MoveTowards(henchman.position.x, henchmanPos.position.x, t), Mathf.MoveTowards(henchman.position.y, henchmanPos.position.y, t), Mathf.MoveTowards(henchman.position.z, henchmanPos.position.z, t));     
+        }
+        else{
+            Moving = true;
+        }
+    }
+        
+}
